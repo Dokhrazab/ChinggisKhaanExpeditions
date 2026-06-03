752f1a96caa56e83282caab53bf9b0a584d14448
Dokhrazab <Bzrgurosa@gmail.com>
2026-06-03 14:16:41 +0800

    Fix: Align sitemap.js with updated itinerary data structure

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,5 +1,5 @@
 // /app/sitemap.js
-import { expeditions, sampleBlogs } from '../data/tours';
+import { expeditions, sampleBlogs } from '../data/itinerary';
 
 export default function sitemap() {
   const baseUrl = 'https://chinggiskhaanexpeditions.com';
@@ -12,8 +12,8 @@ export default function sitemap() {
     priority: 1,
   }));
 
-  // Expedition routes - fixed to use expeditions.en
-  const expeditionRoutes = expeditions.en.map((tour) => ({
+  // Expedition routes - expeditions is now an array
+  const expeditionRoutes = expeditions.map((tour) => ({
     url: `${baseUrl}/expeditions/${tour.id}`,
     lastModified: new Date(),
     changeFrequency: 'monthly',
